dc15a6c2201829cea8b93079741f7166dedeaf94
Vicente <vicente@vicente-HP-Pavilion-dm4-Notebook-PC.(none)>
2012-05-29 10:49:33 -0400

    first commit start project daf

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Daf::Application.config.secret_token = 'e7d5a37fb99fdfdd2e51cf200354ee14eead41ecf06ee104e68fd6e6a3e742ee309b0ab3aa3a40dce15f96f7011a61e1dd5f16be29533774c2e5b21e95fd73e7'
